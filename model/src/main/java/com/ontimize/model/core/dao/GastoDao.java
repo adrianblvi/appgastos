0f7615f4d92d4df067a277a66944d0602919a5c9
adrianblvi <adrianblancovi@gmail.com>
2020-07-28 15:04:52 +0200

    created table gasto and daos

---
+++ model/src/main/java/com/ontimize/model/core/dao/GastoDao.java
@@ -0,0 +1,23 @@
+package com.ontimize.model.core.dao;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+
+import com.ontimize.jee.server.dao.common.ConfigurationFile;
+import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
+
+@Repository("GastoDao")
+@Lazy
+@ConfigurationFile(configurationFile = "dao/GastoDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
+
+public class GastoDao extends OntimizeJdbcDaoSupport {
+	public static final String ATTR_ID = "ID";
+	public static final String ATTR_ID_PARTE = "ID_PARTE";
+	public static final String ATTR_CONCEPT = "CONCEPT";
+	public static final String ATTR_STARTDATE = "STARTDATE";
+	public static final String ATTR_COMMENTS = "COMMENTS";
+	public static final String ATTR_CATEGORY = "CATEGORY";
+	public static final String ATTR_AMOUNT = "AMOUNT";
+	public static final String ATTR_PRICE = "PRICE";
+	public static final String ATTR_PHOTO = "PHOTO";
+}
--- model/src/main/java/com/ontimize/model/core/dao/ParteDao.java
+++ model/src/main/java/com/ontimize/model/core/dao/ParteDao.java
@@ -14,7 +14,6 @@ public class ParteDao extends OntimizeJdbcDaoSupport {
 	public static final String ATTR_DESCRIPTION = "DESCRIPTION";
 	public static final String ATTR_STARTDATE = "STARTDATE";
 	public static final String ATTR_COMMENTS = "COMMENTS";
-	public static final String ATTR_GASTO = "GASTO";
 	public static final String ATTR_STATUS = "STATUS";
 	public static final String ATTR_ENDDATE = "ENDDATE";
 	public static final String QUERY_DETAILS = "details";
