42dafa9c61e3826e62a541842821754d4a9624da
adrianblvi <adrianblancovi@gmail.com>
2020-08-05 13:48:30 +0200

    modfication in home-new-gasto component

--- frontend/src/main/ngx/src/app/main/home/home-gasto-new/home-gasto-new.component.ts
+++ frontend/src/main/ngx/src/app/main/home/home-gasto-new/home-gasto-new.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { OFormComponent, OComboComponent, OIntegerInputComponent } from 'ontimize-web-ngx';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
+import { OFormComponent, OComboComponent, OIntegerInputComponent, OCurrencyInputComponent } from 'ontimize-web-ngx';
 import { isUndefined } from 'util';
-
+import { ActivatedRoute, Params } from '@angular/router';
 @Component({
   selector: 'app-home-gasto-new',
   templateUrl: './home-gasto-new.component.html',
@@ -11,16 +11,22 @@ export class HomeGastoNewComponent implements OnInit {
 
   @ViewChild('oForm')
   private oForm: OFormComponent;
+  @ViewChild('ID_PARTE')
+  private ID_PARTE : OIntegerInputComponent;
   @ViewChild('CATEGORY')
   private CATEGORY: OComboComponent;
   @ViewChild('AMOUNT')
   private AMOUNT: OIntegerInputComponent;
   @ViewChild('PRICE')
-  private PRICE: OIntegerInputComponent;
-  constructor() { }
-  ngOnInit() {
+  private PRICE: OCurrencyInputComponent;
+  constructor(private activatedRoute: ActivatedRoute) {
+    let id_aux = this.activatedRoute.snapshot.params['IDPARTE'];
+    
   }
-
+  ngOnInit() {
+    this.ID_PARTE.setValue(this.activatedRoute.snapshot.params['IDPARTE']);
+    console.log(this.ID_PARTE.getValue());
+  }
   valueChange(event) {
     let a = this.CATEGORY.getSelectedRecord()['PRICE']
     if (isUndefined(a)) {
--- frontend/src/main/ngx/src/app/main/home/home-new/home-new.component.ts
+++ frontend/src/main/ngx/src/app/main/home/home-new/home-new.component.ts
@@ -10,6 +10,7 @@ export class HomeNewComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
