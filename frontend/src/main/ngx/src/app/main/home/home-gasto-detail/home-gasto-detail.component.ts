ce696a615bd6919a12f763012d2c940b8ba22beb
adrianblvi <adrianblancovi@gmail.com>
2020-08-31 09:59:50 +0200

    Added full screen button to spending photo

--- frontend/src/main/ngx/src/app/main/home/home-gasto-detail/home-gasto-detail.component.ts
+++ frontend/src/main/ngx/src/app/main/home/home-gasto-detail/home-gasto-detail.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { OImageComponent } from 'ontimize-web-ngx';
+import { isUndefined } from 'util';
 @Component({
   selector: 'app-home-gasto-detail',
   templateUrl: './home-gasto-detail.component.html',
   styleUrls: ['./home-gasto-detail.component.scss']
 })
 export class HomeGastoDetailComponent implements OnInit {
+  @ViewChild('PHOTO')
+  private PHOTO: OImageComponent;
+
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  onFormDataLoaded(event) {
+    if (isUndefined(this.PHOTO.getValue())) {
+      this.PHOTO.fullScreenButton = false;
+    } else {
+      this.PHOTO.fullScreenButton = true;
+    }
+  }
+  onPhotoChange(event) {
+    if (isUndefined(this.PHOTO.getValue())) {
+      this.PHOTO.fullScreenButton = false;
+    } else {
+      this.PHOTO.fullScreenButton = true;
+    }
+  }
 }
